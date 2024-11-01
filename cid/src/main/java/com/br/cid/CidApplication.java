b7176dd0f38693e721be5971ee03afc8efc3c22a
luiz bernardi <luizbernardi37@gmail.com>
2024-11-01 08:58:47 -0300

    fix add dependences

---
+++ cid/src/main/java/com/br/cid/CidApplication.java
@@ -0,0 +1,13 @@
+package com.br.cid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidApplication.class, args);
+	}
+
+}
---
+++ cid/src/test/java/com/br/cid/CidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
